5b9a6e4a425950d77a66dcaed1db5b0ead8ed37e
Heark <antwauntune25@wcpsstudent.org>
2016-05-16 14:23:21 -0400

    documentation

--- hotdog.js
+++ hotdog.js
@@ -1,27 +1,20 @@
-/** @type {Object} */
+
 var hd = new Object;
 document.addEventListener("error", function(token) {
     console.log("On line" + token.lineNumber);
 });
-/**
-* Write an error to the console
-*
-* @method throwError
-* @param {String} description of thrown Error
-*/
+
 function throwError(err) {
-    /** @type {string} */
     this.err = err;
-    /** @type {Error} */
+
     var ex = new Error(err);
     throw ex;
 }
-/** @type {string} */
 hd.name = "HotDog.js";
 /**
 * Determines whether object is an array or not.
 *
-* @method Object.prototype.isArray
+* @function isArray
 * @return {Boolean} Returns true if object is array
 */
 Object.prototype.isArray = function() {
@@ -34,7 +27,8 @@ Object.prototype.isArray = function() {
 /**
 * Shuffle's array and returns a random output.
 *
-* @method Object.prototype.shuffle
+* @function
+* @name shuffle
 * @return {Object} Returns a random output from array
 */
 Object.prototype.shuffle = function() {
@@ -47,8 +41,19 @@ Object.prototype.shuffle = function() {
 /**
 * Converts a string into a variable
 *
-* @method Object.prototype.toVar
-* @param {Object} The contents of the new variable
+* @function toVar
+* @param {Object} Object - The contents of the new variable
+* @example var User = function(name, job){
+   this.name = name;
+   this.job = job;
+}
+var get_name = prompt("What is your name?") // In this example my name is "Heark"
+var user_name = get_name
+user_name.toVar(new User(user_name, "Newbie"))
+
+console.log(Heark.name);
+console.log(Heark.job);
+
 */
 Object.prototype.toVar = function(c) {
     this.c = c;
@@ -57,9 +62,10 @@ Object.prototype.toVar = function(c) {
 /**
  * Determine if a string contains a substring or if a array contains a value
  * 
- * @method Object.prototype.contains
- * @param {Object} String or Array
+ * @function contains
+ * @param {Object} Object - String or array
  * @return {Boolean} Returns true or false
+ * @example Tea.contains("sugar") // returns true if Tea contains "sugar" string
  */
 Object.prototype.contains = function(substring) {
     if (typeof this === "string") {
